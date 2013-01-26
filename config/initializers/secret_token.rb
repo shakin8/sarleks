953690a3e36828897902198fac494669206cf527
shakin8 <beattie.rory@gmail.com>
2013-01-26 20:28:44 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sarleks::Application.config.secret_token = '5dfe94ccfb5b52817e352c5958c6c1e42ad7d96062c6ccfc1fc1399d8167f5f64e12250a1831e59c7e5763cf409e7705857bf3a70f3c43c47ca7432f41c741de'
