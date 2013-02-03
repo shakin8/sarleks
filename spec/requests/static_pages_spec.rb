13dca129dc3d2cd9310f89d294359cd45f409d9e
shakin8 <beattie.rory@gmail.com>
2013-02-03 15:27:39 +0000

    Added a full_title helper.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,7 +14,14 @@ describe "Static pages" do
       page.should have_selector('title',
       :text => "#{base_title} | Home")
     end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+end
+
   end
+
   describe "Help page" do
     it "should have the h1 'Sarleks Help'" do
       visit '/static_pages/help'
@@ -26,6 +33,7 @@ describe "Static pages" do
       :text => "#{base_title} | Help")
     end
   end
+
   describe "About page" do
     it "should have the h1 'About Sarleks'" do
       visit '/static_pages/about'
