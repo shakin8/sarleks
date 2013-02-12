b9b80330ee6028e2dd1af5ba3e0d0de8d1d85527
shakin8 <beattie.rory@gmail.com>
2013-02-12 20:34:29 +0000

    User sign up capabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+  end
+
   def new
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # user_path can be omitted in a redirect
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Rory Beattie"
+    email "beattie.rory@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
