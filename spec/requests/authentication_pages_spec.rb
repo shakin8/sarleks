80b8577490214d77464cfdae150c6376ca8d1cb5
shakin8 <beattie.rory@gmail.com>
2013-03-16 21:58:22 +0000

    Tweeks on user model

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -129,6 +129,20 @@ describe "Authentication" do
         specify { response.should redirect_to(root_url) }
       end
     end
-  end
+
+    describe "for signed in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "using a 'new' action" do
+        before { get new_user_path }
+        specify { response.should redirect_to(root_path) }
+      end
+
+      describe "using a 'create' action" do
+          before { post users_path }
+          specify { response.should redirect_to(root_path) }            end         
+      end
+    end
 end
 
