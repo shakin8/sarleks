155b887522532a6dce0a8aa1f101a1e432927939
shakin8 <beattie.rory@gmail.com>
2013-07-20 16:22:29 +0100

    reordering

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,7 +37,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_limit => [195, 195]
+    process :resize_to_fill => [200, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
