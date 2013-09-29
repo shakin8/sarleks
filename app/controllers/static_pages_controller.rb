c2a4a18afb127949647ab2ec9edd8bc59a55e7e8
shakin8 <beattie.rory@gmail.com>
2013-09-29 09:56:12 -0400

    leaderboard stuff

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,23 @@
 class StaticPagesController < ApplicationController
   def home
-  	if signed_in?
+    if signed_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
 
   def help
+    @title = "Help"
   end
 
   def about
   end
 
   def contact
+  end
+
+  def leaderboard
+    @title = "Leaderboards"
+    @pieces = Piece.first(10)
   end
 end
